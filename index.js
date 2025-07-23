25bc50ddd3c750464de6d30af8a4ec3f10194d9d
Jaycee Dela Rosa <88520055+Ciarra1@users.noreply.github.com.>
2025-07-23 19:58:02 +0800

    complete mechanics and logic

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-let gridSize;
+
 const generateGridBtn = document.querySelector(".generate-grid-btn");
 const gridDiv = document.querySelector(".grid-container-div");
-function generateGrid(){
-    generateGridBtn.addEventListener("click", function () {
-        gridSize = getGridSize();
+function createGrid(gridSize){
+        
         if(gridSize){
 
             gridDiv.innerHTML = "";
-            console.log(gridSize);
-            const gridWidth = 450;
+
+            const gridWidth = 650;
             let borderSize = 2;
             const boxSize = (gridWidth - (gridSize * borderSize * 2)) / gridSize;
 
@@ -21,23 +20,56 @@ function generateGrid(){
             squares.style.width = boxSize + "px";
             squares.style.height = boxSize + "px";
             squares.style.border = borderSize + "px solid black"; 
-
-
+            hoverEffect(squares);
             gridDiv.appendChild(squares);
+
             }
         } else {
             alert("Missing grid size");
         }
+}
+function getOpacityLevel() {
+    const opacityLevel = (document.getElementById("opacity-level").value);
+    return opacityLevel ? parseFloat(opacityLevel) / 100 : NaN;
+}
+function hoverEffect(squares){
+    let currentOpacity = 0.0;
+    squares.addEventListener("mouseover", function(){
+    let opacityLevel = getOpacityLevel();
+    console.log(opacityLevel);
+    if(currentOpacity > 1){
+        currentOpacity = 1;
+    }
+    if(!isNaN(opacityLevel)){
+        squares.style.backgroundColor = "rgba(0, 0, 0, " + opacityLevel + ")";
+    
+    } else{
+        currentOpacity += 0.15;
+        squares.style.backgroundColor = "rgba(0, 0, 0, " + currentOpacity + ")";
+    }
     })
 }
+
+function clearGrid(){
+    const clearBtn = document.querySelector(".clear-grid-btn");
+    clearBtn.addEventListener("click", function(){
+        const squares = document.querySelectorAll(".grid-container-div div");
+        squares.forEach(square => {
+            square.style.backgroundColor = "";
+
+        })
+    })
+}
+
 function getGridSize(){
     return parseInt(document.getElementById("grid-size").value);
-    
 }
 
 function start(){
-    generateGrid();
+    generateGridBtn.addEventListener("click", function(){
+        createGrid(getGridSize());
+        clearGrid();
+    })
 }
-
-
+createGrid(10)
 start();
